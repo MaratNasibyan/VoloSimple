aa270a0acfe6f4475437ff05f548d55027278f21
Marat Nasibyan ashoti <marat_17.5@live.com>
2016-09-08 22:24:16 +0400

    FirstCommit

---
+++ BookStore/BookStore.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookStore.MVC.Startup))]
+namespace BookStore.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
